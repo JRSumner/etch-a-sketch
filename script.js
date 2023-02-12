211702bf43475176ae072d7a9a91d911454b0863
JRSumner <joeraysumner@gmail.com>
2023-02-12 16:49:40 +0000

    Add logic to resize the pad
    Add button to prompt the user to input a valid grid size, clear previous
    grid and create new grid based on user input. Add alerts to inform the
    user if they have input an invalid value.

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 const container = document.querySelector("#container");
 container.style.display = "flex";
 
-createMultipleColumns(createSingleColumn, 16);
+let numberOfSquares;
 
-function createSingleColumn() {
+createMultipleColumns(createSingleColumn, numberOfSquares);
+addMouseListeners();
+
+function createSingleColumn(numOfColumns = 16) {
   const verticalContainer = document.createElement("div");
 
-  for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < numOfColumns; i++) {
     const verticalGrid = document.createElement("div");
 
     verticalGrid.setAttribute("class", "gridSquare");
-    verticalGrid.style.width = "20px";
-    verticalGrid.style.height = "20px";
+    verticalGrid.style.width = `${480 / numOfColumns}px`;
+    verticalGrid.style.height = `${480 / numOfColumns}px`;
+    verticalGrid.style.backgroundColor = "grey";
     verticalGrid.style.flexDirection = "row";
     verticalGrid.style.borderColor = "black";
     verticalContainer.appendChild(verticalGrid);
@@ -20,27 +24,53 @@ function createSingleColumn() {
   container.appendChild(verticalContainer);
 }
 
-function createMultipleColumns(createSingleColumn, numOfColumns) {
+function createMultipleColumns(createSingleColumn, numOfColumns = 16) {
   for (let i = 0; i < numOfColumns; i++) {
-    createSingleColumn();
+    createSingleColumn(numOfColumns);
   }
 }
 
-const element = document.getElementsByClassName("gridSquare");
+function addMouseListeners() {
+  const element = document.getElementsByClassName("gridSquare");
 
-for (let i = 0; i < element.length; i++) {
-  element[i].addEventListener("mouseover", function () {
-    mouseOver(element[i]);
-  });
-  element[i].addEventListener("mouseout", function () {
-    mouseOut(element[i]);
-  });
+  for (let i = 0; i < element.length; i++) {
+    element[i].addEventListener("mouseover", function () {
+      mouseOver(element[i]);
+    });
+    element[i].addEventListener("mouseout", function () {
+      mouseOut(element[i]);
+    });
+  }
+
+  function mouseOver(element) {
+    element.style.backgroundColor = "blue";
+  }
+
+  function mouseOut(element) {
+    element.style.backgroundColor = "red";
+  }
 }
 
-function mouseOver(element) {
-  element.style.backgroundColor = "blue";
-}
+const btn = document.querySelector("#btn");
 
-function mouseOut(element) {
-  element.style.backgroundColor = "red";
-}
+btn.onclick = () => {
+  numberOfSquares = prompt("Enter number of squares between 1 - 100.");
+
+  if (numberOfSquares > 100) {
+    alert(`The maximum value is 100, you provided ${numberOfSquares}`);
+    return;
+  } else if (numberOfSquares < 1) {
+    alert(
+      `You must provide a value greater than 0, you provided ${numberOfSquares}`
+    );
+  }
+
+  const gridSquare = document.querySelectorAll(".gridSquare");
+
+  for (let i = 0; i < gridSquare.length; i++) {
+    gridSquare[i].remove();
+  }
+
+  createMultipleColumns(createSingleColumn, numberOfSquares);
+  addMouseListeners();
+};
