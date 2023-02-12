2a9be6faeb4a3938fda96161c69645b16083cf84
JRSumner <joeraysumner@gmail.com>
2023-02-12 21:40:06 +0000

    Add html elements and styling
    Add html elements, to structure the header, additional buttons and a
    footer. Add regions to css and style multiple elements.
    
    Add functionality to clear the grid and tie the functionality to a reset
    button.

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const container = document.querySelector("#container");
 container.style.display = "flex";
+container.style.justifyContent = "center";
 
 let numberOfSquares;
-
-createMultipleColumns(createSingleColumn, numberOfSquares);
-addMouseListeners();
 
 function createSingleColumn(numOfColumns = 16) {
   const verticalContainer = document.createElement("div");
@@ -15,9 +13,11 @@ function createSingleColumn(numOfColumns = 16) {
     verticalGrid.setAttribute("class", "gridSquare");
     verticalGrid.style.width = `${480 / numOfColumns}px`;
     verticalGrid.style.height = `${480 / numOfColumns}px`;
-    verticalGrid.style.backgroundColor = "grey";
+    verticalGrid.style.backgroundColor = "#d5d5d5";
     verticalGrid.style.flexDirection = "row";
-    verticalGrid.style.borderColor = "black";
+    verticalGrid.style.border = "0.25px solid grey";
+    verticalGrid.style.borderRadius = "0.5px";
+    verticalGrid.style.boxShadow = "2px 2px 10px #888888";
     verticalContainer.appendChild(verticalGrid);
   }
 
@@ -43,7 +43,7 @@ function addMouseListeners() {
   }
 
   function mouseOver(element) {
-    element.style.backgroundColor = "blue";
+    element.style.backgroundColor = "red";
   }
 
   function mouseOut(element) {
@@ -51,10 +51,13 @@ function addMouseListeners() {
   }
 }
 
-const btn = document.querySelector("#btn");
+const btnSize = document.querySelector("#btn-size");
+const btnReset = document.querySelector("#btn-reset");
 
-btn.onclick = () => {
-  numberOfSquares = prompt("Enter number of squares between 1 - 100.");
+btnSize.onclick = () => {
+  numberOfSquares = prompt(
+    "Enter number between 1 - 100. Example, 16 creates a pad 16 x 16."
+  );
 
   if (numberOfSquares > 100) {
     alert(`The maximum value is 100, you provided '${numberOfSquares}'`);
@@ -63,16 +66,27 @@ btn.onclick = () => {
     alert(
       `You must provide a value greater than 0, you provided '${numberOfSquares}'`
     );
-  } else if (typeof numberOfSquares != "number") {
-    alert(`Please provide a valid number, you provided '${numberOfSquares}'`);
+    return;
   }
 
+  clearGrid();
+  createMultipleColumns(createSingleColumn, numberOfSquares);
+  addMouseListeners();
+};
+
+btnReset.onclick = () => {
+  clearGrid();
+  createMultipleColumns(createSingleColumn, numberOfSquares);
+  addMouseListeners();
+};
+
+function clearGrid() {
   const gridSquare = document.querySelectorAll(".gridSquare");
 
   for (let i = 0; i < gridSquare.length; i++) {
     gridSquare[i].remove();
   }
+}
 
-  createMultipleColumns(createSingleColumn, numberOfSquares);
-  addMouseListeners();
-};
+createMultipleColumns(createSingleColumn, numberOfSquares);
+addMouseListeners();
